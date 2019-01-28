8196a808d6f61676b85c6ce3675ed2f4e783940d
Matei-Marius Micu <micumatei@gmail.com>
2019-01-28 14:57:50 +0200

    Use Mysql as a storage for the API

--- app/disyo/models.py
+++ app/disyo/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class DSApplication(models.Model):
-    name = models.CharField(max_length=1000, primary_key=True)
+    name = models.TextField(max_length=1000, primary_key=True)
     crunchbaseURI = models.URLField()
     githubContributorsCount = models.IntegerField()
     githubLatestCommitDate = models.DateTimeField()
-    githubStars =  models.IntegerField()
+    githubStars = models.IntegerField()
     homepage = models.URLField()
     HQ = models.TextField()
     latestTweetDate = models.DateTimeField()
@@ -17,4 +17,4 @@ class DSApplication(models.Model):
     license = models.TextField()
 
     def __str__(self):
-      return self.name
+        return self.name

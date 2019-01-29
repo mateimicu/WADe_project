9f5ee99b6f454b7783597a2f337b4794c66933c0
Matei-Marius Micu <micumatei@gmail.com>
2019-01-29 04:35:14 +0200

    WIP: frontend first query

--- frontend/src/Query1.js
+++ frontend/src/Query1.js
@@ -4,20 +4,43 @@ import { withRouter } from 'react-router-dom';
 class Query1 extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: '0'};
+    this.state = {
+        githubStars : 0,
+        license: 'NotOpenSource',
+        applicationCategory: "Provisioning",
+        applicationSubCategory: "Automation & Configuration",
+
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   }
 
   handleSubmit(event) {
-    console.log(this.state.value);
+    console.log(event.target)
     var url = "/sparqleditor?";
-    var query = "select * where { ?sub ?pred ?obj.} limit 10";
+    var query = `
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+prefix owl: <http://www.w3.org/2002/07/owl#>
+
+SELECT *
+WHERE {
+   ?subject <https://schema.org/docs/schemaorg.owl#applicationSubCategory> "${this.state.applicationSubCategory}" .
+   ?subject <https://schema.org/docs/schemaorg.owl#applicationCategory> "${this.state.applicationCategory}" .
+   ?subject <http://api.disyo.xyz/api/dsapplications/license> "${this.state.license}" .
+   ?subject <http://api.disyo.xyz/api/dsapplications/githubStars> ?githubStars .
+   FILTER ( ?githubStars >= "${this.state.githubStars}"^^xsd:integer ) .
+}
+LIMIT 100
+`
     var queryUrl = encodeURI(url + query);
+    console.log(queryUrl);
     event.preventDefault();
     this.props.history.push(queryUrl);
     window.location.reload();
@@ -26,12 +49,30 @@ class Query1 extends Component {
   render() {
     return (
       <details open>
-        <summary> descriere query1</summary>
+        <summary> Filter on the popularity of the project.</summary>
         <form onSubmit={this.handleSubmit}>
           <label>
             GithubStars:&nbsp;
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <input name="githubStarts" type="number" value={this.state.githubStars} onChange={this.handleChange} /> 
           </label>
+
+
+          <label>
+            License:&nbsp;
+            <input name="license" type="text" value={this.state.license} onChange={this.handleChange} />
+          </label>
+
+
+          <label>
+            Application Category:&nbsp;
+            <input name="applicationCategory" type="text" value={this.state.applicationCategory} onChange={this.handleChange} />
+          </label>
+
+          <label>
+            Application SubCategory:&nbsp;
+            <input name="applicationSubCategory" type="text" value={this.state.applicationSubCategory} onChange={this.handleChange} />
+          </label>
+
           <input type="submit" value="Run Query" />
         </form>
       </details>

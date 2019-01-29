d24cc65fd4eb7b8b6ce58c7788d7940fc0a526ae
Matei-Marius Micu <micumatei@gmail.com>
2019-01-29 04:34:04 +0200

    Fix the backend url's

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,18 +1,5 @@
 from django.urls import include, path
-from disyo import views
-from django.conf.urls import url
-
-from rest_framework import routers
-from rest_framework_swagger.views import get_swagger_view
-
-
-router = routers.DefaultRouter()
-router.register(r'dsapplications', views.UserViewSet)
-
-schema_view = get_swagger_view(title='Disyo API')
 
 urlpatterns = [
-    url(r'^api-demo/$', schema_view),
-    path(r'api/', include(router.urls)),
     path('', include('disyo.urls')),
 ]
--- app/disyo/urls.py
+++ app/disyo/urls.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from django.urls import include, path
-from disyo.schema import schema
-from graphene_django.views import GraphQLView
+from django.conf.urls import url
 
+from graphene_django.views import GraphQLView
+from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
+
+from disyo.schema import schema
+from disyo import views
+
+
+
+router = routers.DefaultRouter()
+router.register(r'dsapplications', views.UserViewSet)
+
+schema_view = get_swagger_view(title='Disyo API')
 
 urlpatterns = [
+    url(r'^api-demo/$', schema_view),
     path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
+    path(r'', include(router.urls)),
 ]
